92f71d225ac1c78c08ba66e2a2617d0e02ebac0b
Jordan Leslie <jordan02leslie@gmail.com>
2023-12-02 17:15:36 -0500

    progress on prompting

--- functions.js
+++ functions.js
@@ -5,22 +5,17 @@
 import { OPENAI_API_KEY } from "./config.js";
 
 // Make a request to the OpenAI ChatGPT API
-async function fetchData(prompt) {
+async function callGPT(messages) {
   try {
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${OPENAI_API_KEY}`, // Replace with your actual API key
+        Authorization: `Bearer ${OPENAI_API_KEY}`,
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "user",
-            content: prompt,
-          },
-        ],
+        messages: messages,
       }),
     });
 
@@ -37,14 +32,60 @@ async function fetchData(prompt) {
   }
 }
 
-async function call_gpt() {
-  let range = quill.getSelection(true);
-  const text = quill.getText(range.index, range.length);
-  const message = await fetchData(text);
-  if (message) {
-    console.log(message);
-    updateEditor(range, message);
-  }
+async function devilsAdvocate(selectedText) {
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a helpful AI designed to challenge users. The user will input a statement, and you should ask 3 question which challenges the user's statement. \
+        Ask the questions and provide a response to each question in a way that will help the user strengthen their argument. ",
+    },
+    {
+      role: "user",
+      content: selectedText,
+    },
+  ];
+
+  const output = await callGPT(messages);
+  console.log(output);
+}
+
+async function smartFriend(selectedText) {
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a helpful AI designed to help users make their writing less verbose. \
+        the user will input text, and you should revise the text to be less verbose while \
+        retaining the original meaning of the text. If the text is not verbose, do not make any \
+        changes, and respond with the original text only.",
+    },
+    {
+      role: "user",
+      content: selectedText,
+    },
+  ];
+  const output = await callGPT(messages);
+  replaceText(quill.getSelection(true), output);
+}
+
+async function synthesizer(selectedText) {
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a helpful AI designed to help users reduce the redundancy in their writing. \
+        The user will input a text sample, and you should revise the text sample to be less redundant, \
+        without removing any meaning from the text. \
+        If the text contains no redundancies, respond with the original text only.",
+    },
+    {
+      role: "user",
+      content: selectedText,
+    },
+  ];
+  const output = await callGPT(messages);
+  replaceText(quill.getSelection(true), output);
 }
 
 ////////////////////////
@@ -60,43 +101,133 @@ var quill = new Quill("#editor", {
   },
 });
 
-async function updateEditor(range, modelOutput) {
+async function replaceText(range, modelOutput) {
   /*
-  Update editor with suggested message, accept/deny buttons
+  Replace text in editor directly with model output
   */
 
-  const targetLength = range.length;
-  const targetIndex = range.index;
+  let targetLength = range.length;
+  let targetIndex = range.index;
 
-  const totalLength = quill.getLength() - 1;
-
-  modelOutput = "This is a test";
-
-  // TODO: UI for user input accepting/rejecting suggestion, then remove UI after accept/reject
-  let accepted = 1;
-  if (accepted) {
-    // Replace original text with suggestion
-    quill.updateContents(
-      new Delta().retain(targetIndex).delete(targetLength).insert(modelOutput)
-    );
+  if (targetLength === 0) {
+    targetLength = targetIndex;
+    targetIndex = 0;
   }
+
+  // Replace original text with suggestion
+  quill.updateContents(
+    new Delta().retain(targetIndex).delete(targetLength).insert(modelOutput)
+  );
+}
+
+function getSelectedText() {
+  const range = quill.getSelection(true);
+
+  if (range.length !== 0)
+    return {
+      text: quill.getText(range.index, range.length),
+      startIndex: range.index,
+      endIndex: range.index + range.length - 1,
+    };
+
+  // No text selection, call function on entire text window
+  const selectedText = {
+    text: quill.getText(0, range.index),
+    startIndex: 0,
+    endIndex: range.index - 1,
+  };
+
+  if (selectedText.text.length === 0) {
+    displayStatus(status["noSelection"]);
+    return null;
+  }
+  return selectedText;
+}
+
+function showPopup(selectionInfo) {
+  // Get the modal element
+  const modal = document.getElementById("myModal");
+  console.log("selection info: " + selectionInfo);
+  const endBounds = quill.getBounds(selectionInfo);
+  const top = endBounds.bottom + window.scrollY + "px";
+  const left = endBounds.left + window.scrollX + "px";
+
+  // Set the position of the modal
+  modal.style.top = top;
+  modal.style.left = left;
+
+  // Display the text and position info in the modal
+  modal.innerHTML = `<p>Selected Text: ${"i"}</p>
+                     <p>Start Index: ${"am"}</p>
+                     <p>End Index: ${"mad"}</p>`;
+
+  // Display the modal
+  modal.style.display = "block";
+
+  // setTimeout(() => {
+  //   modal.style.display = "none";
+  // }, 3000);
+}
+
+function toggleModal() {
+  const modal = document.getElementById("myModal");
+  modal.classList.toggle("minimized");
+  modal.style.display = modal.classList.contains("minimized")
+    ? "block"
+    : "none";
 }
 
 document
-  .querySelector("#myButton")
+  .querySelector("#synthesizer")
   .addEventListener("click", async function () {
-    const modelOutput = await call_gpt();
-    console.log(modelOutput);
+    const selection = getSelectedText();
+    if (!selection) return;
+    synthesizer(selection.text);
   });
 
 document
-  .querySelector("#myButton1")
+  .querySelector("#smartFriend")
   .addEventListener("click", async function () {
-    const modelOutput = call_gpt();
+    const selection = getSelectedText();
+    if (!selection) return;
+    smartFriend(selection.text);
   });
 
 document
-  .querySelector("#myButton2")
+  .querySelector("#devilsAdvocate")
   .addEventListener("click", async function () {
-    const modelOutput = call_gpt();
+    const selection = getSelectedText();
+    if (!selection) return;
+    const selectedTextEndRange = selection.endIndex;
+    devilsAdvocate(selection.text);
+    showPopup(selectedTextEndRange);
   });
+
+document.querySelector("#undo").addEventListener("click", function () {
+  quill.history.undo();
+});
+
+document.querySelector("#redo").addEventListener("click", function () {
+  quill.history.redo();
+});
+
+document
+  .querySelector("#toggleModalButton")
+  .addEventListener("click", function () {
+    toggleModal();
+  });
+
+const status = {
+  noSelection: { message: "No text selection available", color: "red" },
+  loading: { message: "Loading", color: "blue" },
+};
+
+function displayStatus(status) {
+  const statusBar = document.querySelector("#status-bar");
+  statusBar.textContent = status.message;
+  statusBar.style.backgroundColor = status.color;
+  statusBar.style.visibility = "visible";
+  setTimeout(() => {
+    statusBar.style.visibility = "hidden";
+  }, 2000);
+}
